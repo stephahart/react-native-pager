315dc04fbc90698b3a4139756cab9980d7437d6b
Stephanie Ahart <stephanierahart@gmail.com>
2020-01-23 18:08:03 -0800

    got rid of console log statement

--- src/pager.tsx
+++ src/pager.tsx
@@ -390,11 +390,7 @@ function Pager({
     // the current active child index into a smaller child array
     // TODO: render end of list if index = 0
     const startIndex = moduloJs(activeIndex - 2, numberOfScreens);
-    const endIndex = moduloJs(activeIndex + 10, numberOfScreens);
-
-    console.log(
-      `activeIndex: ${activeIndex}, startIndex: ${startIndex}, endIndex: ${endIndex}`
-    );
+    const endIndex = moduloJs(activeIndex + 6, numberOfScreens);
 
     let adjacentChildren;
     if (startIndex >= endIndex) {
